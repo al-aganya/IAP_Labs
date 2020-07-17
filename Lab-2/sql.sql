d3a68ba7ec9ecfc8c515c73cb4c34fab08a27c47
Al Aganya <al.aganya@strathmore.edu>
2020-07-17 14:28:44 +0300

    sql refactoring

--- Lab-2/sql.sql
+++
@@ -1,7 +0,0 @@
--- Database name=ics102728
-
-CREATE TABLE user(
-id int AUTO_INCREMENT PRIMARY KEY,
-first_name varchar(32) NOT NULL,
-last_name varchar(32),
-user_city varchar(32) );
--- sql.sql
+++ sql.sql
@@ -4,4 +4,6 @@ CREATE TABLE user(
 id int AUTO_INCREMENT PRIMARY KEY,
 first_name varchar(32) NOT NULL,
 last_name varchar(32),
-user_city varchar(32) );
+user_city varchar(32),
+username varchar(20),
+password varchar(255) );
